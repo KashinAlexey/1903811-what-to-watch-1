9e84ed1cbb92ea407eab84d2fa4b329e3fad7aa2
Alexey <apple230379@gmail.com>
2022-04-17 21:58:10 +0300

    Added help command

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,9 @@
 import ImportCommand from './cli-command/import-command.js';
 import CLIApplication from './app/cli-application.js';
+import HelpCommand from './cli-command/help-command.js';
 
 const myManager = new CLIApplication();
 myManager.registerCommands([
-  new ImportCommand
+  new HelpCommand, new ImportCommand
 ]);
 myManager.processCommand(process.argv);
