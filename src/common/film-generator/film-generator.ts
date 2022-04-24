37dab11ab446f3a80ce4f5c704e1a4d179e1054d
Alexey <apple230379@gmail.com>
2022-04-24 17:56:59 +0300

    Add post date generation.

--- src/common/film-generator/film-generator.ts
+++ src/common/film-generator/film-generator.ts
@@ -1,7 +1,10 @@
+import dayjs from 'dayjs';
 import { MockData } from '../../types/mock-data.type.js';
-import { getRandomItem, getRandomItems } from '../../utils/random.js';
+import { generateRandomValue, getRandomItem, getRandomItems } from '../../utils/random.js';
 import { FilmGeneratorInterface } from './film-generator.interface.js';
 
+const FIRST_WEEK_DAY = 1;
+const LAST_WEEK_DAY = 7;
 export default class FilmGenerator implements FilmGeneratorInterface {
   constructor(private readonly mockData: MockData) {}
 
@@ -22,12 +25,13 @@ export default class FilmGenerator implements FilmGeneratorInterface {
     const isFavorite = getRandomItem<string>(this.mockData.isFavorites);
     const videoLink = getRandomItem<string>(this.mockData.videoLinks);
     const previewVideoLink = getRandomItem<string>(this.mockData.previewVideoLinks);
+    const postDate = dayjs().subtract(generateRandomValue(FIRST_WEEK_DAY, LAST_WEEK_DAY), 'day').toISOString();
 
     return [
       name, posterImage, previewImage, backgroundImage, backgroundColor, description, rating,
       scoresCount, director, starrings,
       runTime, genre, released, videoLink, previewVideoLink,
-      isFavorite,
+      postDate, isFavorite,
     ].join('\t');
   }
 }
