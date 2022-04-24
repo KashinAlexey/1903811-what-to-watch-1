c94a724e43077f79426feb459bfbc12bfd6880b1
Alexey <apple230379@gmail.com>
2022-04-24 18:42:55 +0300

    Refactoring. The createMovie function. WIP

--- src/common/file-reader/tsv-file-reader.ts
+++ src/common/file-reader/tsv-file-reader.ts
@@ -1,5 +1,4 @@
 import { readFileSync } from 'fs';
-import { Films } from '../../types/film.type.js';
 import { FileReaderInterface } from './file-reader.interface.js';
 
 export default class TSVFileReader implements FileReaderInterface {
@@ -9,37 +8,5 @@ export default class TSVFileReader implements FileReaderInterface {
 
   public read(): void {
     this.rawData = readFileSync(this.filename, { encoding: 'utf8' });
-  }
-
-  public toArray(): Films {
-    if (!this.rawData) {
-      return [];
-    }
-
-    return this.rawData
-      .split('\n')
-      .filter((row) => row.trim() !== '')
-      .map((line) => line.split('\t'))
-      .map(([title, posterImage, previewImage, backgroundImage, backgroundColor, description, rating,  scoresCount, director, starring, runTime, genre, released, id, videoLink, previewVideoLink, createdDate, isFavorite]) => ({
-        title,
-        posterImage,
-        previewImage,
-        backgroundImage,
-        backgroundColor,
-        description,
-        rating: Number.parseInt(rating, 10),
-        scoresCount: Number.parseInt(scoresCount, 10),
-        director,
-        starring: starring.split(';'),
-        runTime: Number.parseInt(runTime, 10),
-        genre,
-        released: Number.parseInt(released, 10),
-        id,
-        videoLink,
-        previewVideoLink,
-        postDate: new Date(createdDate),
-        isFavorite: isFavorite === 'true',
-      })
-      );
   }
 }
