39f8a5bccae3c913c07cf97be2c0feb8dd317380
Alexey <apple230379@gmail.com>
2022-05-08 17:59:07 +0300

    Extends the UserDocument interface. WIP

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -1,13 +1,18 @@
 import mongoose from 'mongoose';
 import {User} from '../../types/user.type.js';
 
-export interface UserDocument extends User, mongoose.Document {}
+export interface UserDocument extends User, mongoose.Document {
+  createdAt: Date,
+  updatedAt: Date,
+}
 
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   avatarUrl: String,
   token: String,
+}, {
+  timestamps: true,
 });
 
 export const UserModel = mongoose.model<UserDocument>('User', userSchema);
